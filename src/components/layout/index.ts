7f08136275be6f12bbb4bc9a10348a5d9044eb50
Peadar Byrne <peadarjb@gmail.com>
2025-08-14 14:12:00 +0100

    export dashboard layout in index.ts

---
+++ src/components/layout/index.ts
@@ -0,0 +1,5 @@
+// src/components/layout/index.ts
+export { Dashboard } from './Dashboard';
+// export { Header } from './Header';
+// export { LoadingScreen } from './LoadingScreen';
+// export { ResponsiveDashboard } from './ResponsiveDashboard';
